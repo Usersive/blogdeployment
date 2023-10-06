2f4d63492d9439d6b493bc547cd07a899697eecf
Lawal Yusuf Olayinka <94022046+Usersive@users.noreply.github.com>
2023-10-06 23:30:57 +0100

    change files

--- Portfolio/build_files.sh
+++ Portfolio/build_files.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
 # build_files.sh
-#Build the project
-echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
-echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput 
 pip install -r requirements.txt
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
